9f5794195ace9133c99dc26e3cbbc181cd1eeaa6
chetan <chetanpatidar04@gmail.com>
2022-08-25 17:05:26 +0530

    changes for the photo and navbar at the top

--- kksk/settings.py
+++ kksk/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = "/images/downloads/"
+MEDIA_ROOT = BASE_DIR
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
