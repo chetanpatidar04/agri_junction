99c2f5aa6877e19d1d080d2b26b8d213b4958cba
chetan <chetanpatidar04@gmail.com>
2022-08-27 13:12:01 +0530

    changes for the carusel

--- store/views.py
+++ store/views.py
@@ -1,7 +1,17 @@
+from sre_parse import CATEGORIES
+from unicodedata import category
 from django.shortcuts import render
 from .models.product import Product
+from .models.category import Category
 
 
 def index(request):
-    all_products = Product.get_all_products()
-    return render(request,'index.html',{"products":all_products})
+    products = Product.get_all_products()
+    categories = Category.get_all_categories()
+    data = {}
+    data["products"] = products
+    data["categories"] =  categories
+    # return render(request,'orders/orders.html')
+    # print(data["categories"])
+
+    return render(request,'index.html',{"data":data})
