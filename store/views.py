50a9017c55f603dcf378e8a9eefc8e0575c3dd2b
chetan <chetanpatidar04@gmail.com>
2022-08-27 18:16:19 +0530

    changes for the carusel and products based on category

--- store/views.py
+++ store/views.py
@@ -6,12 +6,22 @@ from .models.category import Category
 
 
 def index(request):
-    products = Product.get_all_products()
+    products = None
     categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_category_id(categoryID)
+    else:
+        products = Product.get_all_products()
+
+
     data = {}
     data["products"] = products
     data["categories"] =  categories
+
     # return render(request,'orders/orders.html')
     # print(data["categories"])
 
     return render(request,'index.html',{"data":data})
+
+
