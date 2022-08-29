a10214868c56fd641ea36588728af89fea3d9afe
chetan <chetanpatidar04@gmail.com>
2022-08-29 18:39:49 +0530

    code for validatin in sign up form

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,9 @@
+from email.headerregistry import Address
 from pydoc import describe
 from django.contrib import admin
 from .models import Product
 from .models import Category
+from .models import Customer
 
 class AdminProduct(admin.ModelAdmin):
     list_display=['name',"category","description","price"]
@@ -10,7 +12,11 @@ class AdminProduct(admin.ModelAdmin):
 class AdminCategory(admin.ModelAdmin):
     list_display=['name']
 
+class AdminCustomer(admin.ModelAdmin):
+    list_display=['first_name',"last_name","email","mob_number","address","city","state","pincode"]    
 
+# Register models here to show in admin pannel
 admin.site.register(Product,AdminProduct)
 admin.site.register(Category,AdminCategory)
+admin.site.register(Customer,AdminCustomer)
 
--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',index),
+    path('',index, name="homepage"),
     path('signup',signup),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
