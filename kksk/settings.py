cacbb4443d2e4016e8458d1331df90e18a932362
chetanpatidsar04 <chetanpatidar04@gmail.com>
2022-09-10 17:22:05 +0530

    change for the CSRF issue

--- kksk/settings.py
+++ kksk/settings.py
@@ -18,13 +18,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'jasndjkgbjak'
-SECRET_KEY = environ["SECRET_KEY"]
+SECRET_KEY = 'jasndjkgbjak'
+# SECRET_KEY = environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["agri-junction.herokuapp.com","localhost",]
+ALLOWED_HOSTS = ["agri-junction.herokuapp.com","localhost","127.0.0.1"]
 
 
 # Application definition
@@ -117,10 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =  BASE_DIR / "static"
+# STATIC_ROOT =  BASE_DIR / "static"
 MEDIA_URL = "/images/downloads/"
 MEDIA_ROOT = BASE_DIR
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://agri-junction.herokuapp.com']
