57b4ed4c6e9ffc1562d39b8b749af1536d99e2f2
chetanpatidsar04 <chetanpatidar04@gmail.com>
2022-09-17 13:04:55 +0530

    added remove cart functionlaity

--- store/views/cart.py
+++ store/views/cart.py
@@ -6,6 +6,7 @@ from store.models.product import Product
 
 class Cart(View):
     def get(self, request):
+        print("cart at get",request.session.get('cart'))
         ids = list(request.session.get('cart').keys())
         cart_empty = {}
         cart_empty["flag"] = False
@@ -14,3 +15,11 @@ class Cart(View):
             cart_empty["flag"] = True
             return render(request, "cart.html", {"products": products, "cart_empty": cart_empty})
         return render(request, 'cart.html', {'products': products})
+    
+    def post(self, request):        
+        product_id = request.POST.get("product_id")
+        cart = request.session.get("cart")        
+        del cart[product_id]
+        request.session["cart"] = cart
+        return Cart.get(self,request)
+        
