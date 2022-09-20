5e2b874a3c4ef4bb119cf6eb97143baf71384f78
chetanpatidsar04 <chetanpatidar04@gmail.com>
2022-09-20 14:22:49 +0530

    changes for the alert in post request

--- store/views/home.py
+++ store/views/home.py
@@ -5,59 +5,56 @@ from django.views import View
 
 
 class Index(View):
-    def __init__(self):
-        self.msg = False
-        
+    msg = False
+
     def post(self, request):
-        request.session["msg"] = {}
-        request.session["post"] = {}
+        print("home_post_request")
         product = request.POST.get("product")
         remove = request.POST.get("remove")
         cart = request.session.get("cart")
-        if product:
-            request.session["msg_display"] = True
         if cart:
             quantity = cart.get(product)
             if quantity:
+                Index.msg = False
                 if remove:
                     if quantity <= 1:
+                        Index.msg = True
+                        request.session["msg"] = "Product removed successfully"
                         cart.pop(product)
-                    else:    
+                    else:
                         cart[product] = quantity - 1
                 else:
                     cart[product] = quantity + 1
             else:
-                request.session["msg"] = "Product added successfully"
                 cart[product] = 1
+                Index.msg = True
+                request.session["msg"] = "Product added successfully"
         else:
-            cart = {}
-            request.session["msg"] = "Product added successfully"
+            cart = {}            
             cart[product] = 1
+            Index.msg = True
+            request.session["msg"] = "Product added successfully"
         request.session["cart"] = cart
-        self.msg = True
         return redirect("homepage")
 
-
     def get(self, request):
+        print("home_get_request")
         cart = request.session.get("cart")
         data = {}
-        if not cart:       
+        if not cart:
             request.session["cart"] = {}
-            request.session["msg"] = {}
         products = None
         categories = Category.get_all_categories()
         categoryID = request.GET.get('category')
         data["product_flag"] = True
         if categoryID:
-            products = Product.get_all_products_by_category_id(categoryID)            
-            if not products :
+            products = Product.get_all_products_by_category_id(categoryID)
+            if not products:
                 data["product_flag"] = False
         else:
             products = Product.get_all_products()
         data["products"] = products
-        data["categories"] = categories
-        if not self.msg :
-            if not request.session["msg"]:
-                request.session["msg"] = {}
-
+        data["categories"] = categories        
+        if not Index.msg:
+            request.session["msg"]= {}
         return render(request, 'index.html', {"data": data})
