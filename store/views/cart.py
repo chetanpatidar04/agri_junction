1b26db598b5d8f85b0742e0fe72eeede213e1501
chetanpatidsar04 <chetanpatidar04@gmail.com>
2022-09-20 09:33:30 +0530

    changes for the alert in cart and index page

--- store/views/cart.py
+++ store/views/cart.py
@@ -5,21 +5,27 @@ from store.models.product import Product
 
 
 class Cart(View):
+    def __init__(self):
+        self.cart_msg = False
+
+    def post(self, request):
+        product_id = request.POST.get("product_id")
+        cart = request.session.get("cart")
+        if product_id in list(cart.keys()):
+            self.cart_msg = True
+            request.session["cart_msg"] = "Product removed successfully"
+            del cart[product_id]
+        request.session["cart"] = cart        
+        return Cart.get(self, request)
+    
     def get(self, request):
-        print("cart at get",request.session.get('cart'))
+        if not self.cart_msg :            
+            request.session["cart_msg"] = {}        
         ids = list(request.session.get('cart').keys())
         cart_empty = {}
         cart_empty["flag"] = False
-        products = Product.get_products_by_id(ids)
+        products = Product.get_products_by_id(ids)        
         if len(ids) <= 0:
             cart_empty["flag"] = True
             return render(request, "cart.html", {"products": products, "cart_empty": cart_empty})
         return render(request, 'cart.html', {'products': products})
-    
-    def post(self, request):        
-        product_id = request.POST.get("product_id")
-        cart = request.session.get("cart")        
-        del cart[product_id]
-        request.session["cart"] = cart
-        return Cart.get(self,request)
-        
--- store/views/home.py
+++ store/views/home.py
@@ -5,14 +5,20 @@ from django.views import View
 
 
 class Index(View):
+    def __init__(self):
+        self.msg = False
+        
     def post(self, request):
-        print("home_post_request")
+        request.session["msg"] = {}
+        request.session["post"] = {}
         product = request.POST.get("product")
         remove = request.POST.get("remove")
-        cart = request.session.get("cart")        
+        cart = request.session.get("cart")
+        if product:
+            request.session["msg_display"] = True
         if cart:
             quantity = cart.get(product)
-            if quantity:                
+            if quantity:
                 if remove:
                     if quantity <= 1:
                         cart.pop(product)
@@ -21,19 +27,23 @@ class Index(View):
                 else:
                     cart[product] = quantity + 1
             else:
+                request.session["msg"] = "Product added successfully"
                 cart[product] = 1
         else:
             cart = {}
+            request.session["msg"] = "Product added successfully"
             cart[product] = 1
         request.session["cart"] = cart
+        self.msg = True
         return redirect("homepage")
 
+
     def get(self, request):
-        print("home_get_request")
         cart = request.session.get("cart")
         data = {}
-        if not cart:
+        if not cart:       
             request.session["cart"] = {}
+            request.session["msg"] = {}
         products = None
         categories = Category.get_all_categories()
         categoryID = request.GET.get('category')
@@ -43,7 +53,11 @@ class Index(View):
             if not products :
                 data["product_flag"] = False
         else:
-            products = Product.get_all_products()        
+            products = Product.get_all_products()
         data["products"] = products
         data["categories"] = categories
+        if not self.msg :
+            if not request.session["msg"]:
+                request.session["msg"] = {}
+
         return render(request, 'index.html', {"data": data})
