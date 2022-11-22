b2f343c5654078ea35e40dddc8ccba9709954c76
chetanpatidsar04 <chetanpatidar04@gmail.com>
2022-11-22 19:49:46 +0530

    prepare for deploy

--- kksk/settings.py
+++ kksk/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'jasndjkgbjak'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["agri-junction.herokuapp.com","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh',"localhost","127.0.0.1"]
 
 
 # Application definition
