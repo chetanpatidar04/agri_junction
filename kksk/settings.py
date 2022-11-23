61b3cba1db108fa6c812089cd66e25f5f5ad4d75
chetanpatidsar04 <chetanpatidar04@gmail.com>
2022-11-23 20:49:36 +0530

    change for the deploys

--- kksk/settings.py
+++ kksk/settings.py
@@ -13,7 +13,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print("BASE_DIR",BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -77,8 +76,8 @@ WSGI_APPLICATION = 'kksk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
